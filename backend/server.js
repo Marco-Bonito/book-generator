f2955171afaa6b710dae7220c1411aff1edb682a
Christian Braccio <christianbraccio@gmail.com>
2025-06-10 11:55:13 +0200

    backend config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import env from "/utils/localEnvConfig.js";
 import app from "./src/utils/expressConfig.js";
 import cors from "cors";
-import userRoutes from "/routes/userRoutes.js";
+import userRoutes from "/routes/user_route.js";
+import bookRoutes from "/routes/book_route.js";
+import authRoutes from "/routes/auth_route.js";
 
 const PORT = env.PORT || 3000;
 app.use(
@@ -10,6 +12,8 @@ app.use(
   })
 );
 app.use("/user", userRoutes);
+app.use("/book", bookRoutes);
+app.use("/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta PORT`);
 });
--- backend/src/routes/user_route.js
+++ backend/src/routes/user_route.js
@@ -2,7 +2,5 @@ import { Router } from "express";
 
 const router = Router();
 
-// router.post("/registration" , registration , registrationMiddleware);
-
 
 export default router;
